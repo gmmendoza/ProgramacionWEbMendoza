bbcf4577f4f8a3b740d6acf9ad34301d7d03a95a
SGT <guuamendoza@gmail.com>
2024-06-08 20:41:22 -0300

    actualizacion

---
+++ php/punto1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de Multiplicar</title>
+</head>
+<h1>Tabla de Multiplicar</h1>
+<form method="post" action="">
+
+</form>
+    <?php
+    ?>
+</body>
+</html>
+   
+
