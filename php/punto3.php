e04f1848deab18590035f5e1ba0dc97a8a7ceb03
SGT <guuamendoza@gmail.com>
2024-06-15 19:22:11 -0300

    php

--- php/punto3.php
+++ php/punto3.php
@@ -1,2 +1,18 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Punto 3</title>
+</head>
+<body>
+    <form method="post">
+        <label for="num">Ingresar numero: </label>
+        <input type="text" id="num" name="num" required>
+        <input type="submit" value="convertir">
+    </form>
+    <?php
+    
+    ?>
+</body>
+</html>
